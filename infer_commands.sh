95b5ccf6534ce63cc27a77abc0ed9e4cfacc32fa
Kevin Ladan <kevinladan@outlook.com>
2019-10-07 17:24:45 +0800

    Updated infer commands

--- infer_commands.sh
+++ infer_commands.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+printf "\e[32mDeleting infer-out/ directory.\e[m\n"
+rm -rf infer-out/
+xcodebuild -target CBasediOSApp  -configuration Debug -sdk iphonesimulator clean
 infer run --reactive -- xcodebuild -target CBasediOSApp -configuration Debug -sdk iphonesimulator
